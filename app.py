7a6ff5783dca9349525d5b620c881c57f904c229
pineapple_pizza25 <st10066487@vcconnect.edu.za>
2024-10-01 21:36:49 +0200

    create post with image endpoint

--- app.py
+++ app.py
@@ -18,6 +18,14 @@ class PostData(BaseModel):
     title: str
     body: str
 
+class PostWithImageData(BaseModel):
+    userEmail: str
+    date: str
+    title: str
+    body: str
+    imageBitmap: str  # New field for the base64 image
+
+
 @app.get("/")
 def home():
     return {"MESSAGE": "Hello there" }
@@ -36,6 +44,27 @@ def createPost(post_data: PostData):
         return {"message": "Post created successfully", "post_id": doc_ref.id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+@app.post("/createPostWithImage")
+async def createPost(post_data: PostWithImageData):
+    try:
+        image_data = post_data.imageBitmap
+
+        doc_ref = db.collection("posts")
+        post_ref = doc_ref.add({
+            "user-email": post_data.userEmail,
+            "Date": post_data.date,
+            "title": post_data.title,
+            "body": post_data.body,
+            "image": post_data.imageBitmap   
+        })
+
+        return {"message": "Post created successfully", "post_id": post_ref.id}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/getPosts")
 def getPosts():
